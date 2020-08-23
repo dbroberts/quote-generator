d61d6c6e1223c126968d1861670e49a4e7f0fa9e
David Roberts <dbroberts@gmail.com>
2020-08-23 14:45:15 -0400

    use The Office API for quotes

--- script.js
+++ script.js
@@ -8,24 +8,24 @@ const newQuoteBtn = document.getElementById('new-quote');
 //Get Quote from API
 async function getQuote() {
     const proxyUrl = 'https://cors-anywhere2.herokuapp.com/'
-    const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+    const apiUrl = 'https://officeapi.dev/api/quotes/random';
     try {
         const response = await fetch(proxyUrl + apiUrl);
         const data = await response.json();
-        if (data.quoteAuthor === '') {
+        if (data.data.character.firstname === '') {
             authorText.innerText = "Unknown"
         } else {
-            authorText.innerText = authorText.innerText = data.quoteAuthor;
+            authorText.innerText = authorText.innerText = data.data.character.firstname;
         }
         // Reduce font size for long quotes
-        if (data.quoteText.legth > 50) {
+        if (data.data.content.length > 150) {
             quoteText.classList.add('long-quote')
         } else {
             quoteText.classList.remove('long-quote')
         }
-        quoteText.innerText = data.quoteText;
+        quoteText.innerText = data.data.content;
     } catch (error) {
-        getQuote();
+        //getQuote();
         console.log('whoops, no quote', error);
     }
 }
